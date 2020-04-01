d030034b1aa6232e93be0c3b0492d096ae2031d1
julienbergeron <julien-bergeron@hotmail.com>
2020-04-01 08:26:17 -0400

    mise à jour giignore

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # magic-table-of-content-system
-This script write a table of content, in the top of text file, based on line number of commented sections
+This script write a table of content, in the top of text file, based on line number of commented sections.
--- table_of_content.sh
+++ table_of_content.sh
@@ -1,32 +1,56 @@
 #!/bin/bash
-FILE="/Users/julien/Documents/notes_de_cours_et_cours/module_cours/web/styles/style_livre_html.styl"
+################################################################################
+#Configuration
+################################################################################
+
+#Path of text code file which contain the table of content
+FILE="/Users/julien/scripts/magic-table-of-content-system/example/normalize.scss"
+#head of Table of content
+HEAD_TABLE_OF_CONTENT="//-----Table of content-----//"
+#footer of Table of content
+FOOTER_TABLE_OF_CONTENT="//--@Table of content@--//"
+#marker at the begining of the comment line indicating items for table of content
+TABLE_SECTION_MARKER="//@"
+#comment special character
+COMMENT_SPECIAL_CHARACTER="@"
+#COMMENT_SPECIAL_CHARACTER="@"
+
+#End configuration
+################################################################################
+
+#escape the slash of configuration variables for gsed using ; as a separetor
+TABLE_SECTION_MARKER=$(echo $TABLE_SECTION_MARKER | gsed -e "s;/;\\\/;g")
+HEAD_TABLE_OF_CONTENT=$(echo $HEAD_TABLE_OF_CONTENT | gsed -e "s;/;\\\/;g")
+FOOTER_TABLE_OF_CONTENT=$(echo $FOOTER_TABLE_OF_CONTENT | gsed -e "s;/;\\\/;g")
+
+
 
 echo "---------------------------"
-
-FIRST_LINE_OF_TABLE_OF_CONTENT="$(gsed -n '/\/\/-----Table of content-----\/\//=' $FILE)"
-LAST_LINE_OF_TABLE_OF_CONTENT=$(gsed -n '/\/\/--@Table of content@--\/\//=' $FILE)
-
+FIRST_LINE_OF_TABLE_OF_CONTENT=$(gsed -n "/$HEAD_TABLE_OF_CONTENT/=" $FILE)
+LAST_LINE_OF_TABLE_OF_CONTENT=$(gsed -n "/$FOOTER_TABLE_OF_CONTENT/=" $FILE)
+echo $LAST_LINE_OF_TABLE_OF_CONTENT
 FIRST_LINE_TO_DELETE=$(($FIRST_LINE_OF_TABLE_OF_CONTENT+1))
+echo $FIRST_LINE_TO_DELETE
 LAST_LINE_TO_DELETE=$(($LAST_LINE_OF_TABLE_OF_CONTENT-1))
-
+echo $LAST_LINE_TO_DELETE 
 gsed -i "${FIRST_LINE_TO_DELETE},${LAST_LINE_TO_DELETE}d" $FILE
 
 
 #Firt get the title of the sections
 LINE_NUMBER_OF_COMMENTED_SECTION=()
-LINE_NUMBER_OF_COMMENTED_SECTION+=($(gsed -n '/\/\/\@/=' "$FILE"))
+LINE_NUMBER_OF_COMMENTED_SECTION+=($(gsed -n "/$TABLE_SECTION_MARKER/=" "$FILE"))
 TOTAL_ELEMENT_ARRAY_LINE_NUMBER_OF_COMMENTED_SECTION="${#LINE_NUMBER_OF_COMMENTED_SECTION[@]}"
 
-LAST_LINE_OF_TABLE_OF_CONTENT=$(gsed -n '/\/\/--@Table of content@--\/\//=' $FILE)
-FIRST_LINE_OF_TABLE_OF_CONTENT="$(gsed -n '/\/\/-----Table of content-----\/\//=' $FILE)"
+LAST_LINE_OF_TABLE_OF_CONTENT=$(gsed -n "/$FOOTER_TABLE_OF_CONTENT/=" $FILE)
+FIRST_LINE_OF_TABLE_OF_CONTENT=$(gsed -n "/$HEAD_TABLE_OF_CONTENT/=" $FILE)
 
-#boucle mettre les titres de sections dans une liste
+#sections titles afectation in a list
 j=0
 	until [ ! $j -lt $TOTAL_ELEMENT_ARRAY_LINE_NUMBER_OF_COMMENTED_SECTION ]
 	do
 
 	SECTION_TITLE_WITH_ARROBAS[$j]=$(gsed -n "${LINE_NUMBER_OF_COMMENTED_SECTION[$j]} p" "$FILE")
-	SECTION_TITLE_WITHOUT_ARROBAS[$j]=$(echo "${SECTION_TITLE_WITH_ARROBAS[$j]}" | gsed -e 's/@//g')
+	SECTION_TITLE_WITHOUT_ARROBAS[$j]=$(echo "${SECTION_TITLE_WITH_ARROBAS[$j]}" | gsed -e "s/$COMMENT_SPECIAL_CHARACTER//g")
 
 	echo "${SECTION_TITLE_WITHOUT_ARROBAS[$j]}"
 
@@ -43,14 +67,15 @@ done
 
 #Second write the content
 LINE_NUMBER_OF_COMMENTED_SECOND_SECTION=()
-LINE_NUMBER_OF_COMMENTED_SECOND_SECTION+=($(gsed -n '/\/\/\@/=' "$FILE"))
-FIRST_LINE_OF_TABLE_OF_CONTENT_SECOND="$(gsed -n '/\/\/-----Table of content-----\/\//=' $FILE)"
+LINE_NUMBER_OF_COMMENTED_SECOND_SECTION+=($(gsed -n "/$TABLE_SECTION_MARKER/=" "$FILE"))
+FIRST_LINE_OF_TABLE_OF_CONTENT_SECOND=$(gsed -n "/$HEAD_TABLE_OF_CONTENT/=" $FILE)
 ADD_ONE_TO_FIRST_LINE_OF_TABLE_OF_CONTENT=$(($FIRST_LINE_OF_TABLE_OF_CONTENT_SECOND+1))
 
-#boucle mettre les numéro de ligne dans la table des matières
+#add line numbre to the table of content
 m=0
 		until [ ! $m -lt $TOTAL_ELEMENT_ARRAY_LINE_NUMBER_OF_COMMENTED_SECTION ]
 	do
+	#add line number in the beginning of items from table of content a the dots seperator
 	gsed -i "$((${m}+${ADD_ONE_TO_FIRST_LINE_OF_TABLE_OF_CONTENT}))s/.*/\/\/Ligne : ${LINE_NUMBER_OF_COMMENTED_SECOND_SECTION[$m]}..........&/" $FILE
 
 m=`expr $m + 1`
